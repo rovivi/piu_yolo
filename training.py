2b1535341c0c910dbe230cf6cc54c0032cf6a615
rodrigo vidal <rodrigo.vidalvi@gmail.com>
2026-01-01 15:53:09 -0600

    feat: add initial YOLO dataset including images, labels, and training configuration files.

---
+++ training.py
@@ -0,0 +1,19 @@
+from ultralytics import YOLO
+
+def entrenar():
+    # Cargamos el modelo YOLOv8 Nano (el mejor para apps de escritorio por su velocidad)
+    model = YOLO('yolov8n.pt')
+
+    # Entrenar
+    model.train(
+        data='data.yml',
+        epochs=100,      # Si ves que el "loss" deja de bajar, puedes pararlo antes
+        imgsz=1024,       # Resolución estándar
+        device='mps',        # Usa la GPU si está disponible, si no, CPU
+        batch=16,        # Ajusta según tu RAM
+        project='piu_ia',
+        name='first_try'
+    )
+
+if __name__ == '__main__':
+    entrenar()
